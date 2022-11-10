22533b3ae8d8735c3b203c45583676e54e3f1647
6cpsmidnight <cyberelectro101@gmail.com>
2022-11-10 12:02:36 -0500

    Accidently published old version, publishing new version

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from art import *
 import inquirer
 import time
 import sys
+import keyboard
 
 def cls():
     #clears console
@@ -13,90 +14,59 @@ def cls():
 cls()
 
 # game title
-gameTitleText = """NUMBER
-     GAME"""
-tprint("""------
-NUMBER
-     GAME
-------""", font="big")
+gameTitleTop = """----------------------------------------------------
+||||||||||||||||||||||||||||||||||||||||||||||||||||"""
+gameTitleText = """|NUMBER|
+|||GAME|||"""
+gameTitleBottom = """||||||||||||||||||||||||||||||||||||||||||||||||||||
+----------------------------------------------------\n"""
 
-# animation time interval
-animationTimeInterval = 1
+def gameTitle():
+    print(gameTitleTop)
+    tprint(gameTitleText, font="big")
+    print(gameTitleBottom)
+
+gameTitle()
+
+# assigns the minimum number
+minNum = 0
 
 # loading arrow ascii
-def arrowAnimationFourSeconds():
-    initialLoad = 0
-    while initialLoad < 2:
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                   ______
-                   \    /
-                    \  /
-                     \/ """)
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                      |\ 
-                      | \ 
-                      |  \ 
-                      |___\ """)
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                     /|
-                    / |
-                    \ |
-                     \|""")
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                     ____
-                     |  /
-                     | /
-                     |/""")
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                       /\ 
-                      / \ 
-                     /   \ 
-                    /_____\ """)
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                     ____
-                     \  |
-                      \ |
-                       \|""")
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                       |\ 
-                       | \ 
-                       | /
-                       |/""")
-        time.sleep(animationTimeInterval)
-        cls()
-        tprint(gameTitleText, font="big")
-        sys.stdout.write("""                        /|
-                       / |
-                      /  |
-                     /___|""")
-        initialLoad += 1
-        time.sleep(animationTimeInterval)
-    cls()
+def fixedLoadingAnimation(animationTimeInterval, gameLevel):
+    animationLoadingNum = 0
+    # TODO: make loading time random
+    if gameLevel == 0:
+        # assigns random number between 0-3 as the animation loading time
+        animationLoadingTime = int(random.random() * 3)
+    elif gameLevel == 1:
+        # assigns random number between 1-5 as the animation loading time
+        animationLoadingTime = int(random.random() * 4) + 1
+    elif gameLevel == 2:
+        # assigns random number between 3-5 as the animation loading time
+        animationLoadingTime = int(random.random() * 3) + 2
 
-time.sleep(3)
-arrowAnimationFourSeconds()
+    while animationLoadingNum < 2:
+        sys.stdout.write("\rloading |")
+        time.sleep(animationTimeInterval)
+        sys.stdout.write("\rloading /")
+        time.sleep(animationTimeInterval)
+        sys.stdout.write("\rloading -")
+        time.sleep(animationTimeInterval)
+        sys.stdout.write("\rloading \\")
+        time.sleep(animationTimeInterval)
+        animationLoadingNum += 1
+    sys.stdout.write("\r")
+
+# game difficulty options
+gameDifficultyOptions = [
+    inquirer.List("difficulty",
+            message="Pick a level",
+            choices=["EASY", "NORMAL", "HARD"],
+    ),
+]
 
 def numberGame():
     # choose game difficulty prompt
-    gameDifficultyOptions = [
-        inquirer.List("difficulty",
-                message="Pick a level",
-                choices=["EASY", "NORMAL", "HARD"],
-        ),
-    ]
     gameDifficultyPrompt = inquirer.prompt(gameDifficultyOptions)
     # picks a random number to guess
     if gameDifficultyPrompt == {"difficulty": "EASY"}:
@@ -104,22 +74,30 @@ def numberGame():
         num = int(random.random() * 11)
         # assigns the maximum number
         maxNum = 10
+        # loading animation
+        fixedLoadingAnimation(0.1, 0)
     elif gameDifficultyPrompt == {"difficulty": "NORMAL"}:
         # assigns a random number between 0-100
         num = int(random.random() * 101)
         # assigns the maximum number
         maxNum = 100
+        # loading animation
+        fixedLoadingAnimation(0.1, 1)
     elif gameDifficultyPrompt == {"difficulty": "HARD"}:
         # assigns a random number between 0-1000
         num = int(random.random() * 1001)
         # assigns the maximum number
         maxNum = 1000
+        # loading animation
+        fixedLoadingAnimation(0.1, 2)
     # assigns the minimum and maximum guessed number
     lowNum = 0
     highNum = maxNum
-    # game prompts
+    # restarts number of guesses to 0
+    numberOfGuesses = 0
+    # game input prompt
     guessInputPrompt = "Guess the number (" + str(lowNum) + " to " + str(highNum) + "):\n"
-    playAgainInputPrompt = str(num) + " is the correct number! Play again?[Y/n]"
+    
     # initial game prompt
     guess = input(guessInputPrompt)
     
@@ -127,10 +105,8 @@ def numberGame():
     while i == 0:
         if len(guess) == 0:
             guess = input("Please enter a number between " + str(lowNum) + " to " + str(highNum) + ":\n")
-        elif guess.isdigit() == False:
-            guess = input("Only numbers are allowed, try again:\n")
-        elif int(guess) < lowNum or int(guess) > maxNum:
-            guess = input("Only numbers between 0 to " + str(maxNum) + ", try again:\n")
+        elif guess.isdigit() == False or int(guess) < minNum or int(guess) > maxNum:
+            guess = input("Only numbers between 0 to " + str(maxNum) + " are allowed, try again:\n")
         elif int(guess) == num or int(guess) == num - 1 and highNum == num or int(guess) == num + 1 and lowNum == num: #lowNum == num and highNum == num:
             # stops game
             i =+ 1
@@ -138,7 +114,7 @@ def numberGame():
             j = 0;
             while j == 0:
                 # play again prompt
-                playAgain = input(playAgainInputPrompt)
+                playAgain = input("\n----------------------------------------------------\n" + str(num) + " is the correct number!\nYou won after " + str(numberOfGuesses + 1) + " guesses\nPlay again?[Y/n]")
                 if str(playAgain).isalpha() == False and len(playAgain) > 0:
                     # play again prompt
                     j = 0
@@ -154,7 +130,10 @@ def numberGame():
                     j = 1
                     # clears console
                     cls()
-                    print("Thank you for playing!")
+                    # game title
+                    gameTitle()
+                    prompt = input("Thank you for playing!\nIf at any time you would like to continue,\npress the enter key on your keyboard\n")
+                    numberGame()
         elif int(guess) <= num and int(guess) >= lowNum:
             lowNum = int(guess) + 1
             guess = input("Too low of a guess, try again (" + str(lowNum) + " to " + str(highNum) + "):\n")
@@ -167,6 +146,7 @@ def numberGame():
             guess = input("Too high of a guess, try again (" + str(lowNum) + " to " + str(highNum) + "):\n")
         else:
             print("Error, try again!")
+        numberOfGuesses += 1
 
 # initial game ran
 numberGame()
